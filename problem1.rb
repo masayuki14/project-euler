5d909aaca166ad8804da92ea435a01f4901223bc
Masayuki Morisaki <masayuki144@gmail.com>
2013-08-03 17:23:07 +0900

    コメントを整形

--- problem1.rb
+++ problem1.rb
@@ -1,8 +1,8 @@
-#Multiples of 3 and 5
-#Problem 1
+# Multiples of 3 and 5
+# Problem 1
 #
-#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-#Find the sum of all the multiples of 3 or 5 below 1000.
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
 #
 # 10未満の自然数で3または5の倍数は3,5,6,9である。
 # これらを合計すると23となる。
