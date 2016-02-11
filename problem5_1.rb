fbba31d0c1e5ff7be3ebd168beb4e2838d3f6591
Masayuki Morisaki <masayuki144@gmail.com>
2016-02-11 22:54:18 +0900

    make new problems.

---
+++ problem5_1.rb
@@ -0,0 +1,12 @@
+# Smallest multiple
+# Problem 5
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+#
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+#
+# 2520は1から10のすべての数で割り切れる最小値である。
+# 1から20の全てで割り切れる最小の正の数は何か。
+
+# 最小公倍数を求める問題
+
+
